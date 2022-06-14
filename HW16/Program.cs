d88629674081569f115e8f5a743bdf3895c45e3d
SvetlanaGIP <gip1@acn.su>
2022-06-14 12:38:09 +1000

    Добавьте файлы проекта.

---
+++ HW16/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace HW16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter data for 5 products");
+            const int n = 5;
+            Product[] products = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Please enter product code:");
+                String ProductIdRaw = Console.ReadLine();
+                int productId = System.Convert.ToInt32(ProductIdRaw);
+
+                Console.Write("Please enter name:");
+                string name = Console.ReadLine();
+
+                Console.Write("Please enter price:");
+                String priceRaw = Console.ReadLine();
+                double price = System.Convert.ToDouble(priceRaw);
+                
+
+                products[i] = new Product() { ProductId = productId, Name = name, Price = price };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(products);
+            
+            using (StreamWriter sw =new StreamWriter("../../../Products.json"))
+            {
+                sw.WriteLine(jsonString);
+            }
+            
+        }
+
+        
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr=new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            //string json = File.ReadAllText("Products.json");
+            //Product[] products = JsonSerializer.Deserialize<Product[]>(json);
+
+            Product maxProduct = products[0];
+            //String productName = "";
+            foreach (Product p in products)
+            {
+                if (p.Price > maxProduct.Price)
+                {
+                    maxProduct = p;
+                }
+                    
+            }
+
+            Console.WriteLine($"{maxProduct.ProductId}{maxProduct.Name}{maxProduct.Price}");
+
+            Console.Write("press any key to exit");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
